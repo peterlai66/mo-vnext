97af2c10615e87adb56f3e2692870224ef2b3d47
Peter Lai <peterlai@live.com>
2026-03-25 06:58:40 +0800

    chore(debug): 印出 request path

--- src/index.ts
+++ src/index.ts
@@ -369,6 +369,7 @@ async function getReplyText(
   export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
 		console.log("[fetch] hit");
+		console.log("[fetch] path", new URL(request.url).pathname);
 		const url = new URL(request.url);
   
 	  if (url.pathname === "/api/line/webhook" && request.method === "POST") {
